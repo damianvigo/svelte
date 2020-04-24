6f817597481577bbe5fe04da290f5aaf81ff8597
Damian <damiandanielvigo@gmail.com>
2020-04-24 01:39:53 -0300

    reactividad

---
+++ src/components/About.svelte
@@ -0,0 +1,22 @@
+<script>
+  let someText = `Frontend Developer`;
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<style>
+  p {
+    color: var(--theme-color);
+  }
+</style>
+
+<div class="About">
+  <!-- separar en estructura con div, recomendado  -->
+  <p>{someText}</p>
+  <button on:click={handleClick} role="button">
+    Click {count === 0 ? '' : count}
+  </button>
+</div>
